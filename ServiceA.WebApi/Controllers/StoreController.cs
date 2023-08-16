cca8ecc0873fc21784edc33d06c42fd9b3d8159d
zanuccod <zanuccod@gmail.com>
2023-08-16 19:46:43 +0000

    Added Service-A as SecretManagement

---
+++ ServiceA.WebApi/Controllers/StoreController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(IConfiguration config, ILogger<StoreController> logger)
+        {
+            _config = config;
+            _logger = logger;
+        }
+
+        [HttpGet("GetValueFromKey")]
+        public async Task<ActionResult<string>> GetValueFromKey(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return BadRequest("Give a valid key");
+            }
+
+            var value = _config[key];
+            return string.IsNullOrEmpty(value)
+                ? await Task.FromResult(NotFound(key))
+                : await Task.FromResult(Ok(_config[key]));
+        }
+    }
+}
---
+++ ServiceA.WebApi/Program.cs
@@ -0,0 +1,40 @@
+using Dapr.Client;
+using Dapr.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+/*
+ *************************************************************
+ * If we would like to add the secret store to the configuration
+ *************************************************************
+ */
+builder.Configuration
+    .AddDaprSecretStore("secret-store", new DaprClientBuilder().Build());
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddSeq(serverUrl: "http://localhost:80");
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapDefaultControllerRoute();
+app.MapControllers();
+
+app.Run();
