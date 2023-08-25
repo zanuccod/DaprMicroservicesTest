c5f345b25f3f44e20b0f6e61125802cce5e8298a
qxz1jly <Davide.Zanucco@partner.bmw.de>
2023-08-25 12:50:53 +0200

    Minor changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,16 +39,19 @@ resource "azurerm_key_vault_secret" "kv-redisuser-key" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = var.key_vault_secret-redis-username-key
   value        = var.key_vault_secret-redis-username-value
+  depends_on   = [azurerm_key_vault.kv]
 }
 
 resource "azurerm_key_vault_secret" "kv-redispassword-key" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = var.key_vault_secret-redis-password-key
   value        = var.key_vault_secret-redis-password-value
+  depends_on   = [azurerm_key_vault.kv]
 }
 
 resource "azurerm_key_vault_secret" "kv-redisinstance-key" {
   key_vault_id = azurerm_key_vault.kv.id
   name         = var.key_vault_secret-redis-instance-key
   value        = var.key_vault_secret-redis-instance-value
+  depends_on   = [azurerm_key_vault.kv]
 }
